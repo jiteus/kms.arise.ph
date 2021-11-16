25e4af80a52a8efe6fd088e870ad084a5dfc7fce
Joan Irma Teus <joanirmateus@gmail.com>
2021-11-16 15:12:56 +0800

    testing title

--- arise-app/src/index.js
+++ arise-app/src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <h1>ArisePH Member Portal</h1>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
